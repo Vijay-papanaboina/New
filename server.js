8739a7cf31e294c71c45b8a2b6cb9c85fee125f4
Vijay Papanaboina <vijaypapanaboina3@gmail.com>
2024-10-24 16:05:42 +0530

    removed

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const port = 3000;
 // Body parser middleware
 app.use(express.json());
 
-// Serve static files from the 'public' folder
+// Serves static files from the 'public' folder
 app.use(express.static('public'));
 
-// Razorpay instance
+// Creates Razorpay instance
 const razorpay = new Razorpay({
     key_id: process.env.YOUR_KEY_ID,
     key_secret: process.env.YOUR_KEY_SECRET
@@ -27,14 +27,14 @@ app.post('/create-order', async (req, res) => {
     };
     try {
         const order = await razorpay.orders.create(options);
-        res.json(order); // Send the order details to the frontend
+        res.json(order); // Sends the order details to the frontend
     } catch (error) {
         console.error(error);
         res.status(500).send("Error creating order");
     }
 });
 
-// Start the server
+// Starting the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
